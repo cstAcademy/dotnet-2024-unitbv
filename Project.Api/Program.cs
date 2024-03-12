fbcd68c4fb60493f703b10b0760135ad3fc90f53
Alexandra Donici <adonici@transnetyx.com>
2024-03-12 15:49:55 +0200

    Add swagger

--- Project.Api/Program.cs
+++ Project.Api/Program.cs
@@ -7,22 +7,21 @@ internal class Program
         // Add services to the container.
 
         builder.Services.AddControllers();
+        builder.Services.AddSwaggerGen();
 
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
-
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
         app.UseHttpsRedirection();
 
         app.UseAuthorization();
 
         app.MapControllers();
-
-        //if (app.Environment.IsDevelopment())
-        //{
-        //    app.UseSwagger();
-        //    app.UseSwaggerUI();
-        //}
 
         app.Run();
     }

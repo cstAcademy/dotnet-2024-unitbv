46eec36ca51fb303ce3a73c8132c0275f8c279f3
Alexandra Donici <adonici@transnetyx.com>
2024-03-12 15:28:12 +0200

    Add project files.

---
+++ Project.Api/Program.cs
@@ -0,0 +1,29 @@
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        //if (app.Environment.IsDevelopment())
+        //{
+        //    app.UseSwagger();
+        //    app.UseSwaggerUI();
+        //}
+
+        app.Run();
+    }
+}
